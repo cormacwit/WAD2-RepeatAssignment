8296aabc60b91c77907090dbc31233333f4db80b
cormacwit <20093840@mail.wit.ie>
2023-12-22 08:03:06 +0000

    Upcoming movies from TMDB

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -190,3 +190,22 @@ export const getTVReviews = (tv_id) => {
       return json.results;
     });
 };
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+import fetch from 'node-fetch';
+
+export const getUpcomingMovies = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+        );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
