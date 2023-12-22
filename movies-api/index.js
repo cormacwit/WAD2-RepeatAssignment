09246852815d3ae582754903e565c2055d167bc9
cormacwit <20093840@mail.wit.ie>
2023-12-22 07:53:23 +0000

    Added Movies Route

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
 import './db';
+import moviesRouter from './api/movies';   //import movies router
 import defaultErrHandler from './errHandler'
 
 dotenv.config();
@@ -13,7 +14,9 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.use(defaultErrHandler);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
